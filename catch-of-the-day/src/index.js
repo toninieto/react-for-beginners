5783319b770ddb5ab7855951163d68b15fee8482
Toni Nieto <toni@tresipunt.com>
2017-02-16 19:35:40 +0100

    Chapter 5 completed, loaded css into application

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,6 +1,7 @@
 // let's go!
 import React from 'react';
 import { render } from 'react-dom'; // We import only the render method
+import './css/style.css'
 
 import StorePicker from './components/StorePicker';
 
