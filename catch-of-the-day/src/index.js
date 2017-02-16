c04a785a8eccc8da60e3e25350a19323eccd9989
Toni Nieto <toni@tresipunt.com>
2017-02-16 19:22:17 +0100

    Added all the files from catch of the day and completed until the chapter 3.
    First component created.

---
+++ catch-of-the-day/src/index.js
@@ -0,0 +1,7 @@
+// let's go!
+import React from 'react';
+import { render } from 'react-dom'; // We import only the render method
+
+import StorePicker from './components/StorePicker';
+
+render(<StorePicker/>, document.querySelector('#main'));
