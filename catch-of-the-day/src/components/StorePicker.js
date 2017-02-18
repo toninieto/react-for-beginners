50389be6b0d08a19d3d41c7b3df0d5f806d51db7
Toni Nieto <toni@tresipunt.com>
2017-02-18 17:30:57 +0100

    Chapter 11 completed

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -2,15 +2,24 @@ import React from 'react';
 import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
-	render() {
-		return (
-			<form className="store-selector">
-				<h2>Please Enter a store</h2>
-				<input type="text" required placeholder="Store Name" defaultValue={getFunName()} />
-				<button type="submit">Visit Store</button>
-			</form>
-		)
-	}
+
+    goToStore(event) {
+        event.preventDefault();
+        console.log('You changed the URL')
+        // First grab the text from the box
+        console.log(this.storeInput.value)
+        // Second we're going to transition from / to /store/:storeID
+    }
+
+    render() {
+        return (
+            <form className="store-selector" onSubmit={(e) => this.goToStore(e)}>
+                <h2>Please Enter a store</h2>
+                <input type="text" required placeholder="Store Name" defaultValue={getFunName()} ref={(input) => {this.storeInput = input}} />
+                <button type="submit">Visit Store</button>
+            </form>
+        )
+    }
 }
 
 export default StorePicker;
